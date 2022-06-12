82899472a998748ab516f7d6c7e4c4966d9ca180
Ahmet Atakan <atakanahmetce@gmail.com>
2022-06-12 22:59:58 +0300

    try

--- test.py
+++ test.py
@@ -1,38 +1,18 @@
-import networkx as nx
-import time
-
-from adhoccomputing.GenericModel import GenericModel
-from adhoccomputing.Generics import Event, EventTypes, ConnectorTypes
 from adhoccomputing.Experimentation.Topology import Topology
-from adhoccomputing.Networking.LinkLayer.GenericLinkLayer import GenericLinkLayer
 from adhoccomputing.Networking.LogicalChannels.GenericChannel import GenericChannel
-from node_model import *
 from node_model.GenericNode import UsrpNode
-
-"""class AdHocNode(GenericModel):
-  def on_message_from_top(self, eventobj: Event):
-    self.send_down(Event(self, EventTypes.MFRT, eventobj.eventcontent))
-
-  def on_message_from_bottom(self, eventobj: Event):
-    self.send_up(Event(self, EventTypes.MFRB, eventobj.eventcontent))
-
-  def __init__(self, componentname, componentinstancenumber):
-    super().__init__(componentname, componentinstancenumber)"""
-
 
 def main():
   topo = Topology()
   topo.construct_winslab_topology_with_channels(4, UsrpNode, GenericChannel)
 
   topo.start()
-  #for i in range(5):
+  
   from_id = 1
   dest_id = 2
   message = "hw!"
 
   topo.nodes[from_id].appl.send_down(message, dest_id)
-
-  #time.sleep(0.25)
 
 if __name__ == '__main__':
   main()
